ca92a1bd3acaf503c8245e22fb2d90897a77aaa6
rossedfort <ross.edfort@wazeedigital.com>
2017-02-03 08:52:16 -0700

    add styles - start app

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,26 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import MyTitle from './MyTitle'
+import { render } from 'react-dom'
+import '../public/normalize.css'
+import '../public/style.css'
 
-var MyFirstComponent = React.createClass({
-  render: function () {
+const App = React.createClass({
+  render () {
     return (
-      // props are passed in as 'attributes' on your jsx tag
-      // they are by reference
-      // jsx tags are closing
-      <div>
-        <MyTitle title='props are cool' color='rebeccapurple' />
-        <MyTitle title='lololololololo' color='aliceblue' />
-        <MyTitle title='yayayayayayaya' color='peru' />
+      <div className='app'>
+        <div className='landing'>
+          <h1>svideo</h1>
+          <input type='text' placeholder='Search' />
+          <a>or Browse All</a>
+        </div>
       </div>
     )
   }
 })
 
-ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
-
-// React.createClass: creates a template for a component
-// React.createElement: creates an instance of a component based on the template
-// React.createFactory: easier way of creating a bunch of components
-// createComponent calls must only return exactly one component otherwise you are writing invalid js
-  // i.e. return x,y
+render(<App />, document.getElementById('app'))
