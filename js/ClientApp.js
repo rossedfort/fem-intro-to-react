ca1ce793f5bce4b08670c9100919e0d91a640517
rossedfort <ross.edfort@wazeedigital.com>
2017-02-03 09:39:44 -0700

    add the router

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,18 +1,23 @@
+// Libs
 import React from 'react'
 import { render } from 'react-dom'
+import { HashRouter, Match } from 'react-router'
+// Styles
 import '../public/normalize.css'
 import '../public/style.css'
+// Components
+import Landing from './Landing'
+import Search from './search'
 
 const App = React.createClass({
   render () {
     return (
-      <div className='app'>
-        <div className='landing'>
-          <h1>svideo</h1>
-          <input type='text' placeholder='Search' />
-          <a>or Browse All</a>
+      <HashRouter>
+        <div className='app'>
+          <Match exactly pattern='/' component={Landing} />
+          <Match pattern='/search' component={Search} />
         </div>
-      </div>
+      </HashRouter>
     )
   }
 })
