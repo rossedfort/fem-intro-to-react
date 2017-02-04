0d12e97adff5c8b8ab8a1bff8cd73eb83f0d546c
rossedfort <rossedfort@gmail.com>
2017-02-04 10:20:19 -0700

    add tests

--- js/Search.js
+++ js/Search.js
@@ -4,14 +4,32 @@ import preload from '../public/data.json'
 import ShowCard from './ShowCard'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
+  },
   render () {
     return (
       <div className='search'>
-        {preload.shows.map((show) => {
-          return (
-            <ShowCard key={show.imdbID} {...show} />
-          )
-        })}
+        <header>
+          <h1>svideo</h1>
+          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='Search' />
+        </header>
+        <div>
+          {preload.shows.filter((show) => {
+            return `${show.title} ${show.description}`
+                .toUpperCase()
+                .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+          }).map((show) => {
+            return (
+              <ShowCard key={show.imdbID} {...show} />
+            )
+          })}
+        </div>
       </div>
     )
   }
